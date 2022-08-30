ad0b6bbc690683ac872235b62b2b4bf3e4eba0a0
indrajith76 <indrajithgoswami@gmail.com>
2022-08-30 16:01:40 +0600

    random user commit

--- random-user-api/js/index.js
+++ random-user-api/js/index.js
@@ -11,9 +11,17 @@ const displayUsers = (user) => {
             <img src='${user.picture.large}' class='rounded-circle border border-5 border-primary' style="width:15%;">
             <div>
                 <h3 class='text-primary'>Name : ${user.name.title} ${user.name.first} ${user.name.last}</h3>
+                <h5 class='text-secondary'>Country : ${user.location.country}</h5>
+                <h5 class='text-secondary'>City : ${user.location.city}</h5>
+                <h5 class='text-secondary'>Phone : ${user.phone}</h5>
+                <h5 class='text-secondary'>Email : ${user.email}</h5>
             </div>
             <div>
-                <h3 class='text-primary'>Name : ${user.name.title} ${user.name.first} ${user.name.last}</h3>
+                <h6 class='text-secondary'>Street : ${user.location.street.name} ${user.location.street.number}</h6>
+                <h6 class='text-secondary'>Time Zone : ${user.location.timezone.description}</h6>
+                <h6 class='text-secondary'>Post Code : ${user.location.postcode}</h6>
+                <h6 class='text-secondary'>Date of Birth : ${user.dob.date.slice(0,10)}</h6>
+                <h6 class='text-secondary'>Date of Birth : ${user.login.username}</h6>
             </div>
         </div>
     `;
