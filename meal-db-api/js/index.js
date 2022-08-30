39247444927d3dfbeb7b79296775724330b54934
indrajith76 <indrajithgoswami@gmail.com>
2022-08-30 20:39:39 +0600

    done meal api

--- meal-db-api/js/index.js
+++ meal-db-api/js/index.js
@@ -6,6 +6,7 @@ const loadMeals = (meal) => {
 
 const displayMeals = (meals) => {
     const foodsContainer = document.getElementById('foods-container');
+    foodsContainer.textContent = ``;
     meals.forEach(meal => {
         const foodDiv = document.createElement('div');
         foodDiv.classList.add('col');
@@ -17,7 +18,7 @@ const displayMeals = (meals) => {
                     <p class="card-text">${meal.strInstructions.slice(0, 80)}</p>
                 </div>
                 <div class='details-div'>
-                    <button class="btn btn-outline-primary my-auto">Details</button>
+                    <button onclick="loadFoodDetails('${meal.idMeal}')" class="btn btn-outline-primary my-auto">Details</button>
                 </div>
             </div>
         `
@@ -26,4 +27,39 @@ const displayMeals = (meals) => {
 }
 
 
-loadMeals('f')
+const loadFoodDetails = async(mealId) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayFoodDetails(data.meals[0]);
+}
+
+const displayFoodDetails = meals => {
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.innerHTML = `
+        <div class='d-flex'>
+        <img src='${meals.strMealThumb}'/>
+        <div class='mt-5 ms-3 me-5'>
+            <h2>Food Name: ${meals.strMeal}</h2>
+            <h5 class='text-secondary'>Category : ${meals.strCategory}</h5>
+            <h5 class='text-secondary'>It's ${meals.strArea} food.</h5>
+            <p><strong>Descriptions:</strong> ${meals.strInstructions.slice(0,400)}</p>
+            <button class='btn btn-danger'>Order Now</button>
+        </div>
+        </div>
+        <button onclick='cancelDetails()' class='cancel'>✖</button>
+    `;
+}
+
+const cancelDetails = () => {
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.innerHTML = ``;
+}
+
+document.getElementById('search-btn').addEventListener('click', function(){
+    const inputField = document.getElementById('input-field');
+    loadMeals(inputField.value);
+    console.log(inputField.value);
+})
+
+loadMeals('')
